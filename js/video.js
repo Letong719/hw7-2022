1f8804c051e48b90a6f0d643adb8ecfb9aaf0431
Letong719 <121955096+Letong719@users.noreply.github.com>
2023-04-04 19:35:37 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -9,7 +9,6 @@ document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	vid = document.getElementById("player1"); 
 	vid.play();
-	// Update volume value
 	var volumeInfo = document.getElementById("volume");
 	var volumePercent = (vid.volume * 100).toFixed(0);
 	volumeInfo.innerText = volumePercent + "%";
@@ -17,24 +16,24 @@ document.querySelector("#play").addEventListener("click", function() {
 
 document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video");
-	vid = document.getElementById("player1"); 
+	vid = document.getElementById("player1");
 	vid.pause();
 });
 
 document.querySelector("#slower").addEventListener("click", function() {
-	vid = document.getElementById("player1"); 
+	vid = document.getElementById("player1");
 	vid.playbackRate -= 0.1;
 	console.log("New speed is " + vid.playbackRate);
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
-	vid = document.getElementById("player1"); 
+	vid = document.getElementById("player1");
 	vid.playbackRate += 0.1;
 	console.log("New speed is " + vid.playbackRate);
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
-	vid = document.getElementById("player1"); 
+	vid = document.getElementById("player1")
 	newTime = vid.currentTime + 10;
 	if (newTime >= vid.duration) {
 		vid.currentTime = 0;
@@ -70,7 +69,6 @@ document.querySelector("#vintage").addEventListener("click", function(){
     vid = document.getElementById("player1");
 	vid.classList.add("oldSchool");
 	console.log("Old School style");
-	
 });
 
 document.querySelector("#orig").addEventListener("click", function(){
